f399a0aaa7f07f90d465b68a2cd74c88e1a7369b
Dmitry G. Anderson <dandgerson@gmail.com>
2022-10-02 17:55:08 +0300

    js tools

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Stafford',
+      name: "Luna",
+      animal: "Dog",
+      breed: "Stafford",
     }),
     React.createElement(Pet, {
-      name: 'Chuck',
-      animal: 'Dog',
-      breed: 'Stafford',
+      name: "Chuck",
+      animal: "Dog",
+      breed: "Stafford",
     }),
     React.createElement(Pet, {
-      name: 'Cluck',
-      animal: 'Dog',
-      breed: 'Stafford',
+      name: "Cluck",
+      animal: "Dog",
+      breed: "Stafford",
     }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.querySelector('#root'));
+render(React.createElement(App), document.querySelector("#root"));
