442bf941e8ab184ba3befe7efcfaf2177794e6b4
Dmitry G. Anderson <dandgerson@gmail.com>
2022-10-05 04:04:29 +0300

    clickOutside

--- src/Modal.js
+++ src/Modal.js
@@ -1,17 +1,27 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
+const Modal = ({ clickOutside, children }) => {
   const elRef = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
+  const handleClickOutside = (e) => {
+    if (e.target.id === "modal") {
+      clickOutside();
+    }
+  };
+
   useEffect(() => {
     const modalRoot = document.querySelector("#modal");
     modalRoot.appendChild(elRef.current);
+    modalRoot.addEventListener("click", handleClickOutside);
 
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      modalRoot.removeChild(elRef.current);
+      modalRoot.removeEventListener("click", handleClickOutside);
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
